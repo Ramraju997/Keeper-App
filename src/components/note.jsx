d8a9063b9a0444686e15f6afc5878b8a51fb4475
Ram Raju <84369007+Ramraju997@users.noreply.github.com>
2023-07-26 08:25:24 +0530

    Add files via upload

---
+++ src/components/note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Note = () => {
+    return(
+        <div className="note">
+            <h1>note heading</h1>
+            <p contentEditable="true">note content</p>
+        </div>
+    );
+}
+
+export default Note;
